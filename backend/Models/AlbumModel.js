3a4d428514e3c9225483da8149342ba77e639159
kj <pinchv@gmail.com>
2016-07-31 20:21:50 +0300

    Доделал админку

--- backend/Models/AlbumModel.js
+++ backend/Models/AlbumModel.js
@@ -4,7 +4,16 @@ module.exports = function(pool) {
 			pool.query('INSERT INTO Albums SET ?', key, callback);
 		},
 		GetAlbumsByWeek: function(key, callback) {
-			pool.query('SELECT * FROM Albums where WeekNumber=? order by dateCreate desc', key, callback);
+			pool.query('SELECT * FROM Albums WHERE WeekNumber=? order by dateCreate desc', key, callback);
+		},
+		GetAlbumById: function(key, callback) {
+			pool.query('SELECT * FROM Albums WHERE id=?', key, callback);
+		},
+		EditAlbumSave: function(key, callback) {
+			pool.query('UPDATE Albums SET ? WHERE id=?', key, callback);
+		},
+		DeleteAlbum: function (key, callback) {
+			pool.query('DELETE FROM Albums WHERE id=?', key, callback);
 		},
 	};
 
