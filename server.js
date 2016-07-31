d66c82cdaaf7fbd59c2d59b99b35161f581755b1
kj <pinchv@gmail.com>
2016-07-31 17:31:44 +0300

    Доделал часть админки

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var qs = require('querystring');
+var internetradio = require('node-internet-radio');
+var basicAuth = require('basic-auth-connect');
 var mysql = require('mysql');
 var conf = require('./backend/config');
 var pool = mysql.createPool(conf);
 var rating = require('./backend/Models/RatingModel')(pool);
-var bodyParser = require('body-parser');
-var internetradio = require('node-internet-radio');
+var album = require('./backend/Models/AlbumModel')(pool);
 var utils = require('./backend/Utils');
-var basicAuth = require('basic-auth-connect');
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -64,10 +66,45 @@ app.post('/Rating/Save', function (req, res) {
 ///АДМИНИСТРАТИВНАЯ ЧАСТЬ///
 var auth = basicAuth('Ivan', 'EgorLetov@!');
 app.get('/RadioAdmin/', auth, function (req, res) {
-  res.render('Admin.ejs');
+  album.GetAlbumsByWeek(1, function (err, al) {
+    if (!err)
+      res.render('Admin.ejs', { Albums: al });
+  });
 });
 
-///СТРАНИЦА НЕДЕЛИ
+app.post('/RadioAdmin/Get', auth, function (req, res) {
+  album.GetAlbumsByWeek(req.body.week, function (err, al) {
+    if (!err)
+      res.render('AdminPartial.ejs', { Albums: al });
+  });
+});
+
+app.post('/RadioAdmin/Save', auth, function (req, res) {
+  try {
+    if (req.body.album != null) {
+      var reqAlbum = qs.parse(req.body.album);
+
+      if (reqAlbum.IsVisible)
+        reqAlbum.IsVisible = true;
+      else
+        reqAlbum.IsVisible = false;
+
+      reqAlbum.dateCreate = new Date();
+      reqAlbum.id = utils.Guid();
+
+      album.SaveAlbum(reqAlbum, function (error, data) {
+        if (!error)
+          res.json({ type: 'success' });
+        else
+          res.json({ type: 'error' });
+      });
+    }
+  } catch (e) {
+    res.json({ type: 'error' });
+  }
+});
+
+///СТРАНИЦА НЕДЕЛИ///
 app.get('/weeks', function (req, res) {
   //
   var items = [
